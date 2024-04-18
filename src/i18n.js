70c29302d3a715e3487355bc329640ebcaf04d12
damarisfreg <fregosodmar24@gmail.com>
2024-04-17 23:04:36 -0700

    Implementing nested translation keys

--- src/i18n.js
+++ src/i18n.js
@@ -7,16 +7,31 @@ import {initReactI18next} from "react-i18next";
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
     debug: true,
-    lng: "en",
+    fallbackLng: "en",
+    returnObjects: true, 
     resources: {
       en: {
         translation: {
-          welcome: "Welcome to Afforable Building Maintenance"
+          welcome: "Welcome to Afforable Building Maintenance",
+          description: {
+            slogan: "Where no job is too big or too small!",
+            year: "Since 2019",
+            line1: "Sick of all the problems that your home is giving you?",
+            line2: "Get your household troubles resolved in a breeze",
+            line3: "with quailty home repair and maintenance sercices from trusted experts!"
+          },
         },
       },
       es: {
         translation: {
-          welcome: "Bienvenido al Afforable Building Maintenace"
+          welcome: "Bienvenido al Afforable Building Maintenace",
+          description: {
+            slogan: "¡Donde ningún trabajo es demasiado grande o demasiado pequeño!",
+            year: "Desde 2019",
+            line1: "¿Estás harto de todos los problemas que te está dando tu hogar?",
+            line2: "Resuelva sus problemas domésticos en un abrir y cerrar de ojos",
+            line3: "¡Con servicios de calidad de reparación y mantenimiento del hogar por parte de expertos confiables!"
+          },
         },
       }
     }
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,16 +3,18 @@ import { useTranslation } from "react-i18next";
 export default function WelcomePage() {
   const { t } = useTranslation()
 
+  const {slogan, year, line1, line2, line3} = t("description");
+
   return (
     <div className="WelcomePage">
       <div className="opening">
         <h1>{t("welcome")}</h1>
-        <p>Where no job is too big or too small!</p>
-        <h4>Since 2019</h4>
+        <p>{slogan}</p>
+        <h4>{year}</h4>
       </div>
-      <p>Sick of all the problems that your home is giving you?</p>
-      <p>Get your household troubles resolved in a breeze</p>
-      <p> with quailty home repair and maintenance sercices from trusted experts!</p>
+      <p>{line1}</p>
+      <p>{line2}</p>
+      <p>{line3}</p>
 
     </div>
   );
