20bf7c4748c937d3780a3de95a1fba015d91ce05
damarisfreg <fregosodmar24@gmail.com>
2024-04-18 23:23:32 -0700

    creating files for each language

--- src/i18n.js
+++ src/i18n.js
@@ -3,36 +3,11 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 //this is letting i18n know this is a React app
 import {initReactI18next} from "react-i18next";
+import Backend from "i18next-http-backend"
 
 
-i18n.use(LanguageDetector).use(initReactI18next).init({
+i18n.use(LanguageDetector).use(initReactI18next).use(Backend).init({
     debug: true,
     fallbackLng: "en",
     returnObjects: true, 
-    resources: {
-      en: {
-        translation: {
-          welcome: "Welcome to Afforable Building Maintenance",
-          description: {
-            slogan: "Where no job is too big or too small!",
-            year: "Since 2019",
-            line1: "Sick of all the problems that your home is giving you?",
-            line2: "Get your household troubles resolved in a breeze",
-            line3: "with quailty home repair and maintenance sercices from trusted experts!"
-          },
-        },
-      },
-      es: {
-        translation: {
-          welcome: "Bienvenido al Afforable Building Maintenace",
-          description: {
-            slogan: "¡Donde ningún trabajo es demasiado grande o demasiado pequeño!",
-            year: "Desde 2019",
-            line1: "¿Estás harto de todos los problemas que te está dando tu hogar?",
-            line2: "Resuelva sus problemas domésticos en un abrir y cerrar de ojos",
-            line3: "¡Con servicios de calidad de reparación y mantenimiento del hogar por parte de expertos confiables!"
-          },
-        },
-      }
-    }
 })
--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import "./i18n.js"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router><App /></Router>
+    <React.Suspense fallback="loading...">
+      <Router><App /></Router>
+    </React.Suspense>
   </React.StrictMode>
 );
 
