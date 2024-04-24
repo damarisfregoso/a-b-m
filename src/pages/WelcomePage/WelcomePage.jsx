6335929aea16d47e5ab4bb268f5e920295c9ac36
damarisfreg <fregosodmar24@gmail.com>
2024-04-23 23:57:06 -0700

    Styling and adding to welcomePage

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -5,18 +5,35 @@ export default function WelcomePage() {
   const { t } = useTranslation()
 
   const {slogan, year, line1, line2, line3} = t("description");
+  const {ourServ} = t("navbar");
 
   return (
     <div className="WelcomePage">
       <div className="opening">
         <h1>{t("welcome")}</h1>
-        <p>{slogan}</p>
+        <span>{slogan}</span>
         <h4>{year}</h4>
       </div>
-      <p>{line1}</p>
-      <p>{line2}</p>
-      <p>{line3}</p>
-
+      <div className="quote">
+        <p>{line1}</p>
+        <p>{line2}</p>
+        <p>{line3}</p>
+      </div>
+      <h1 className="chooseUs">{t("chooseUs")}</h1>
+      <div className="chooseUs-Box">
+        <>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex obcaecati numquam aspernatur eaque doloremque, temporibus, ipsa culpa atque repellendus veniam quas possimus facere earum omnis dolores laboriosam quidem maxime unde.
+          Molestias commodi modi vel ducimus exercitationem eligendi tempora? Facere enim sunt possimus officiis dolorem cum qui molestias tempora deleniti, quod temporibus distinctio necessitatibus doloribus saepe dicta dolorum perspiciatis accusamus amet!
+          Blanditiis libero fuga cumque facilis! Ad est fugit natus neque reprehenderit porro deleniti eos tempore reiciendis voluptatum saepe eius id veniam ut deserunt, accusamus repellendus quae et? Impedit, unde nulla?</p>
+        </>
+        <>
+          <img src="https://static.scientificamerican.com/sciam/cache/file/D1E779C2-5585-48B9-B5B8CA83481D212C_source.jpg?w=1200" alt="placeholder for now" />
+        </>
+      </div>
+      <div className="ourServices">
+        <h1 className="chooseUs">{ourServ}</h1>
+        <p>{t("servicesQuote")}</p>
+      </div>
     </div>
   );
 }
