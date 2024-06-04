dfc81a3b5a5d294556c4f68b429781b744b2206f
damarisfreg <fregosodmar24@gmail.com>
2024-06-03 23:55:50 -0700

    Adding in service cards

--- src/pages/OurServicesPage/OurServicesPage.jsx
+++ src/pages/OurServicesPage/OurServicesPage.jsx
@@ -8,6 +8,12 @@ export default function OurServiesPage() {
   return (
     <div className="OurServicesPage">
       <h1>{serviceTitle}</h1>
+      <div className="card">
+    <img className="card-top" src="https://www.pennington.com/all-products/fertilizer/resources/-/media/Project/OneWeb/Pennington/Images/blog/fertilizer/8-Steps-to-Growing-a-Healthy-Indoor-Garden-Anytime/tropical-urban-garden.jpg" alt="Card image cap"/>
+    <div className="card-body">
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
     </div>
   );
 }
