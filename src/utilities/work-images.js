0be132c3f89575ef253b30d3429999b7a3faa78a
damarisfreg <fregosodmar24@gmail.com>
2024-06-12 23:00:26 -0700

    Updating translation files

---
+++ src/utilities/work-images.js
@@ -0,0 +1,12 @@
+const workImages = [
+  'https://imgur.com/I2V8aHk',
+  'https://imgur.com/K9HALs1',
+  'https://imgur.com/zKQKvfz',
+  'https://imgur.com/Mjavpsq',
+  'https://imgur.com/BjqKObk',
+  'https://imgur.com/Xay7Ant',
+  'https://imgur.com/jkadUyS',
+  'https://imgur.com/wgpsr8D'
+]
+
+export default workImages;
